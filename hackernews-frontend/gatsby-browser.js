17afbd542cbc7287a7dbfffd7257f35536cb23be
fajri <fajrizulfikar85@gmail.com>
2023-01-05 17:35:51 +0700

    feat: create header

--- hackernews-frontend/gatsby-browser.js
+++ hackernews-frontend/gatsby-browser.js
@@ -1,9 +1,1 @@
-import React from "react";
-
-import Layout from "./src/components/layout";
-
 import "./src/styles/global.css";
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>;
-};
--- hackernews-frontend/src/pages/index.js
+++ hackernews-frontend/src/pages/index.js
@@ -1,12 +1,70 @@
 import * as React from "react";
 
-import Layout from "../components/layout";
+import Hackernews from "../gifs/hackernews.gif";
+
+const menus = ["new", "past", "comments", "ask", "show", "jobs", "submit"];
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </Layout>
+    <div>
+      <div
+        className="navbar h-14 min-h-0"
+        style={{ backgroundColor: "#ff6600" }}
+      >
+        <div className="navbar-start lg:w-0">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="#fff"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <a className="btn btn-ghost normal-case border border-white">
+            <img src={Hackernews} alt="hackernews logo" />
+          </a>
+        </div>
+        <div className="navbar hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <span className="font-bold">Hacker News</span>
+            </li>
+            {menus.map((menu, index) => (
+              <li className="flex flex-row items-center">
+                <a>{menu}</a>
+                {index !== menus.length - 1 && "|"}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn btn-link text-white">Login</a>
+        </div>
+      </div>
+    </div>
   );
 };
 
